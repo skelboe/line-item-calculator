fbcc4c4f63fa6c970f4323ebb29d7cd30291fff9
Lars Skelboe <lars@skelboe.dk>
2020-01-27 20:38:05 +0100

    Remove border from input

--- components/input.jsx
+++ components/input.jsx
@@ -5,7 +5,7 @@ export default ({ value, setValue }) => {
             pattern="[0-9]*"
             value={value}
             onChange={e => setValue(e.target.value)}
-            className="bg-color-200 border-b pr-20 px-4 pt-10 pb-4 leading-none text-5xl font-thin text-gray-700 focus:text-black focus:bg-yellow-100 focus:outline-none"
+            className="appearance-none bg-color-200 pr-20 px-4 pt-10 pb-4 leading-none text-5xl font-thin text-gray-700 focus:text-black focus:bg-yellow-100 focus:outline-none"
         />
     )
 }
