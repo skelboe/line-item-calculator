a2a891dec043151c82e83ba32a33962ef200dec5
Lars Skelboe <lars@skelboe.dk>
2020-02-03 14:25:54 +0100

    Use number instead of tel
    Even though the telephone keypad was nice, you could not add
    decimals values.

--- components/input.jsx
+++ components/input.jsx
@@ -1,8 +1,7 @@
 export default ({ value, setValue }) => {
     return (
         <input
-            type="tel"
-            pattern="[0-9]*"
+            type="number"
             value={value}
             onChange={e => setValue(e.target.value)}
             className="appearance-none bg-color-200 pr-20 px-4 pt-10 pb-4 leading-none text-5xl font-thin text-gray-700 focus:text-black focus:bg-yellow-100 focus:outline-none"
